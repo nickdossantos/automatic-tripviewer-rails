687f4eb7e5400eaaaa846749f06766101e6b61bb
Nate Klaiber <nate@theklaibers.com>
2015-02-24 10:15:10 -0500

    Unicorn socket to use with nginx

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,8 @@ worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
 
+listen ENV.fetch('UNICORN_SOCKET', "/tmp/tripviewer-rails.sock"), :backlog => 64
+
 before_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
