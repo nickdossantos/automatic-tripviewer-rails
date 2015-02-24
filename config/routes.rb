315a085c2f06cbe0a00347b90faaf81106d32327
Nate Klaiber <nate@theklaibers.com>
2015-02-24 13:45:39 -0500

    Rough in routes
    The routes are not implemented. This was to get
    the rendering in place.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
+  get '/download/trips(.:format)', to: 'trips#download', as: 'download_trips'
+
   get '/login', to: 'pages#login'
+  scope(controller: :sessions) do
+    get '/login', action: 'create'
+    get '/logout', action: 'destroy'
+  end
+
+  resources :trips, only: [:index, :show]
+  resources :vehicles, only: [:index, :show]
 end
