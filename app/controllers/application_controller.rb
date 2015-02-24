730559cae2e950973caef3e65c4e8957f83e67b1
Nate Klaiber <nate@theklaibers.com>
2015-02-24 13:35:22 -0500

    Add helper_methods for local access_token ENV var

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+  def access_token
+    ENV['AUTOMATIC_ACCESS_TOKEN']
+  end
+  helper_method :access_token
+
+  def access_token?
+    access_token.present?
+  end
+  helper_method :access_token?
 end
