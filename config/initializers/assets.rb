1ef2a81bb5616701330200fe87088e7f1c14b7d8
Nate Klaiber <nate@theklaibers.com>
2015-02-24 10:53:51 -0500

    Move bootstrap to the application.scss

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bo
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( bootstrap/dist/css/bootstrap.min.css )
+# Rails.application.config.assets.precompile += %w( bootstrap/dist/css/bootstrap.min.css )
