22621b7dd1a61a67afa250ba50def1bf8bbb622e
Nate Klaiber <nate@theklaibers.com>
2015-02-24 13:33:25 -0500

    Separate the home page and login pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
+  # This returns an overall summary of the
+  # trips collection.
   def home
+    render layout: false
+  end
+
+  # This starts the OAuth process. It will
+  # be skipped if there is an access token
+  # provided in the ENV.
+  def login
     render layout: false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
+
+  get '/login', to: 'pages#login'
 end
