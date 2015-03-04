033b2284a2355775b391b85d3ca6e9e7ec8e92a4
Nate Klaiber <nate@theklaibers.com>
2015-03-04 16:11:57 -0500

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
+  get '/login', to: 'pages#login'
+  get '/auth/:provider/callback', to: 'pages#create_session', as: 'automatic_auth'
+
   get '/download/trips(.:format)', to: 'trips#download', as: 'download_trips'
 
-  get '/login', to: 'pages#login'
   scope(controller: :sessions) do
     get '/login', action: 'create'
     get '/logout', action: 'destroy'
   end
 
   resources :trips, only: [:index, :show]
-  resources :vehicles, only: [:index, :show]
+  resources :vehicles, only: [:index]
 end
