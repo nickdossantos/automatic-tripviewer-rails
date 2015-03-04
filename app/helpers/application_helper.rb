eff66c40ba7a78416e0109ee04c5a58a65ac5502
Nate Klaiber <nate@theklaibers.com>
2015-03-04 16:11:08 -0500

    Adding temporary application helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,43 @@
 module ApplicationHelper
+
+  def active_nav(args={})
+    controllers = args.delete(:controllers).split(',').map(&:strip)
+    actions     = args.delete(:actions).split(',').map(&:strip)
+
+    if contains_controller?(controllers) && contains_action?(actions)
+      'active'
+    end
+  end
+
+  def contains_controller?(*controllers)
+    controllers.flatten.include?(controller.controller_name)
+  end
+
+  def contains_action?(*actions)
+    actions.flatten.include?(controller.action_name)
+  end
+
+  def hard_brakes_class(count)
+    if count.to_i > 0
+      "someHardBrakes"
+    else
+      "noHardBrakes"
+    end
+  end
+
+  def hard_accels_class(count)
+    if count.to_i > 0
+      "someHardAccels"
+    else
+      "noHardAccels"
+    end
+  end
+
+  def speeding_class(count)
+    if count.to_i > 0
+      "someSpeeding"
+    else
+      "noSpeeding"
+    end
+  end
 end
