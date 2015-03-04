28b0481ab408e680b81b7efc702dbeb90669e869
Nate Klaiber <nate@theklaibers.com>
2015-03-04 16:26:23 -0500

    Adding the base trips controller
    * Extract request/response details to gem

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,44 @@
+class TripsController < ApplicationController
+  before_filter :verify_access_token!
+
+  def index
+    pagination_params = {
+      :page     => params.fetch(:page, 1),
+      :per_page => params.fetch(:per_page, 10)
+    }
+
+    trips_route   = automatic_routes.route_for('trips')
+    trips_request = automatic_connection.get(trips_route.url_for, pagination_params)
+
+    case(trips_request.status)
+    when 200
+      trips_response   = MultiJson.load(trips_request.body)
+      link_header_data = trips_request.headers['link']
+      link_header      = Automatic::Response::LinkHeader.new(link_header_data)
+      metadata         = Automatic::Response::Metadata.new(trips_response.fetch('_metadata', {}))
+      pagination       = Automatic::Response::Pagination.new(pagination_params.merge!(total_entries: metadata.count))
+      trips            = Automatic::Models::Trips.new(trips_response.fetch('results', []))
+
+      @trips      = trips
+      @pagination = pagination
+    else
+      render_500
+    end
+  end
+
+  def show
+    trip_route   = automatic_routes.route_for('trip')
+    trip_request = automatic_connection.get(trip_route.url_for(id: params[:id]))
+
+    case(trip_request.status)
+    when 200
+      trip_response = MultiJson.load(trip_request.body)
+
+      @trip = Automatic::Models::Trip.new(trip_response)
+    when 404
+      render_404
+    else
+      render_500
+    end
+  end
+end
