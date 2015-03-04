79bc1f3b784b0ba024f693e866e345986c4f63fc
Nate Klaiber <nate@theklaibers.com>
2015-03-04 16:26:51 -0500

    Ensure homepage uses the extracted layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   # This returns an overall summary of the
   # trips collection.
   def home
-    render layout: false
   end
 
   # This starts the OAuth process. It will
