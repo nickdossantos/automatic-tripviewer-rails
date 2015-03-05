7e20f5306ae91126076bcf40f3f3a602705c7128
Nate Klaiber <nate@theklaibers.com>
2015-03-04 21:51:33 -0500

    Show the summary section of the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,13 @@
 class PagesController < ApplicationController
-  # This returns an overall summary of the
-  # trips collection.
+  before_filter :verify_access_token!, only: [:home]
+
   def home
+    trips_route  = automatic_routes.route_for('trips')
+    trip_records = all_trips(trips_route.url_for)
+
+    @trips = Automatic::Models::Trips.new(trip_records)
   end
 
-  # This starts the OAuth process. It will
-  # be skipped if there is an access token
-  # provided in the ENV.
   def login
     render layout: false
   end
