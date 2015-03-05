97ed68cc57abd3ebec599c55f1d36e084f9bf59c
Nate Klaiber <nate@theklaibers.com>
2015-03-04 21:50:40 -0500

    Method to find vehicles by their URL

--- lib/automatic/models/vehicles.rb
+++ lib/automatic/models/vehicles.rb
@@ -13,6 +13,10 @@ module Automatic
         record_collection.each(&block)
       end
 
+      def find_by_url(url)
+        self.select { |r| r.url == url.to_s }.first
+      end
+
       private
       def record_collection
         @collection.map { |r| Vehicle.new(r) }
