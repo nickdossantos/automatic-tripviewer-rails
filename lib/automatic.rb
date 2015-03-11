4c53d79e6e19c920768d18c209548f73da538108
Nate Klaiber <nate@theklaibers.com>
2015-03-11 19:46:49 -0400

    Adding support for additional routes

--- lib/automatic.rb
+++ lib/automatic.rb
@@ -9,10 +9,13 @@ module Automatic
     return @routes if @routes
 
     @routes = RestlessRouter::Routes.new
-    @routes.add_route(RestlessRouter::Route.new('trips', 'https://api.automatic.com/trip/{?page,per_page}', templated: true))
+    @routes.add_route(RestlessRouter::Route.new('trips', 'https://api.automatic.com/trip/{?page,limit}', templated: true))
     @routes.add_route(RestlessRouter::Route.new('trip', 'https://api.automatic.com/trip/{id}/', templated: true))
     @routes.add_route(RestlessRouter::Route.new('vehicles', 'https://api.automatic.com/vehicle/{?page,per_page}', templated: true))
     @routes.add_route(RestlessRouter::Route.new('vehicle', 'https://api.automatic.com/vehicle/{id}/', templated: true))
+    @routes.add_route(RestlessRouter::Route.new('user', 'https://api.automatic.com/user/{id}/', templated: true))
+    @routes.add_route(RestlessRouter::Route.new('user-profile', 'https://api.automatic.com/user/{id}/profile/', templated: true))
+    @routes.add_route(RestlessRouter::Route.new('user-metadata', 'https://api.automatic.com/user/{id}/metadata/', templated: true))
 
     @routes
   end
