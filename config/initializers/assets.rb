090fe6b9638dcd1dd6f6443cdc28fcc80ebe1493
Nate Klaiber <nate@theklaibers.com>
2015-03-31 22:45:13 -0400

    Precompile the fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,9 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
 
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
