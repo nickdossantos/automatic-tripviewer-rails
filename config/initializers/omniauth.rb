f1b87b54e0bb3d854381154ad8c5b774fac3d96e
Nate Klaiber <nate@theklaibers.com>
2015-07-29 12:16:21 -0400

    Explicit list of scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use(OmniAuth::Builder) do
   provider :automatic, ENV.fetch('AUTOMATIC_CLIENT_ID'), ENV.fetch('AUTOMATIC_CLIENT_SECRET'), {
-    provider_ignores_state: true
+    provider_ignores_state: true,
+    scope: "scope:behavior scope:location scope:public scope:trip scope:user:profile scope:vehicle:events scope:vehicle:profile"
   }
 end
